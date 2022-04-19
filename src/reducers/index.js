283fe7bb992a5da9c62bac5b211c73d9751b0adb
hayato_hamabe <hayato.hamabe@teamfactory.jp>
2022-04-19 10:03:45 +0900

    create operationLogs reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux'
 
 import events from './events'
+import operationLogs from './operationLogs'
 
 combineReducers({ events })
 
-export default combineReducers({ events })
+export default combineReducers({ 
+  events,
+  operationLogs,
+})
