5c7ae9b2366c0b42201fa4518374395b1fdc8fa3
hayato_hamabe <hayato.hamabe@teamfactory.jp>
2022-04-21 11:42:09 +0900

    delete all operationLogs

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -2,8 +2,11 @@ import React, { useState, useContext } from 'react'
 import { 
   CREATE_EVENT,
   DELETE_ALL_EVENTS,
+  ADD_OPERATION_LOG,
+  DELETE_ALL_OPERATION_LOGS,
 } from '../actions'
 import AppContext from '../contexts/AppContext'
+import { timeCurrentIso8601 } from '../utils'
 
 const EventForm = () => {
   const {state, dispatch} = useContext(AppContext)
@@ -18,6 +21,13 @@ const EventForm = () => {
       title, 
       body
     })
+
+    dispatch({
+      type: ADD_OPERATION_LOG,
+      description: 'イベントを作成しました。' ,
+      operatedAt: timeCurrentIso8601()
+    })
+
     setTitle('')
     setBody('')
   }
@@ -25,10 +35,26 @@ const EventForm = () => {
   const deleteAllEvents = e => {
     e.preventDefault()
     const result = window.confirm('全てのイベントを本当に削除しても良いですか？')
-    if (result) dispatch({type: DELETE_ALL_EVENTS})
+    if (result) {
+      dispatch({type: DELETE_ALL_EVENTS})
+
+      dispatch({
+        type: ADD_OPERATION_LOG,
+        description: '全てのイベントを削除しました。' ,
+        operatedAt: timeCurrentIso8601()
+      })
+    }
   }
 
   const unCreatable = title === "" || body === ""
+
+  const deleteAllOperationLogs = e => {
+    e.preventDefault()
+    const result = window.confirm('全ての操作ログを本当に削除しても良いですか？')
+    if (result) {
+      dispatch({type: DELETE_ALL_OPERATION_LOGS})
+    }
+  }
 
   return(
     <>
@@ -46,6 +72,7 @@ const EventForm = () => {
 
       <button className='btn btn-primary' onClick={addEvent} disabled={unCreatable}>イベントを作成する</button>
       <button className='btn btn-danger' onClick={deleteAllEvents} disabled={state.events.length === 0}>全てのイベントを削除する</button>
+      <button className='btn btn-danger' onClick={deleteAllOperationLogs} disabled={state.operationLogs.length === 0}>全ての操作ログを削除する</button>
       </form>
     </>
   )
